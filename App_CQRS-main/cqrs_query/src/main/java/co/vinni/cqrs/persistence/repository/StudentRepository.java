5143fa8b173be39f929f80a6a65373ded34517dc
Hejumi28 <jahm2812@gmail.com>
2025-11-17 23:12:45 -0500

    CQRS 2 Bases de Datos
    Implementacion de Base de datos MongoDB atlas para el query

---
+++ App_CQRS-main/cqrs_query/src/main/java/co/vinni/cqrs/persistence/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package co.vinni.cqrs.persistence.repository;
+
+import co.vinni.cqrs.persistence.entity.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StudentRepository extends MongoRepository<Student, Long> {
+}
